0b71629470a627a41349f151beca09a4c3cbc890
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-10 12:15:08 -0700

    use Write funtion ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,13 @@
  *
  * Description: print a quote using the write function
  *
- * Return: 1 (error)
+ * Return: 1 (error).
 */
 
 int main(void)
 {
 	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, st, 59);
 
 	return (1);
