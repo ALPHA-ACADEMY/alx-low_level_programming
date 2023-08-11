b33c0749dcceba786d7c4583d5ea84d184ee2997
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-11 15:41:25 -0700

    print 0-->9 using putchar, but in reverse LOL

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the lowercase alphabet in reverse,
+ *	followed by a new line.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int digit = i + '0';
+
+		putchar(digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
