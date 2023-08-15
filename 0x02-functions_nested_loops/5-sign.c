28c047d06640f0e2a1122e1d89ceb483081a1d87
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-15 07:51:45 -0700

    guess the signe of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number.
+ * @n: the integer te be checked.
+ *
+ * Return: 1 if n is greather than zero,
+ *	0 if n is zero,
+ *	-1 if n less than zero.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
