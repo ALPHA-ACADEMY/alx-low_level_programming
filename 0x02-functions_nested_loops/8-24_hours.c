626f1cb8387bc458690eaaf8a2e732bd028b982e
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-15 09:51:50 -0700

    jack-baure --> the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,22 +9,17 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int hour, minute;
 
-	for (i = 0; i <= 2; i++)
-		for (j = 0; j <= 3; j++)
-			for (k = 0; k <= 5; k++)
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
+	for (hour = 0; hour <= 23; hour++)
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 
-					_putchar('\n');
-
-					if (i == 2 && j == 3 && k == 5 && l == 9)
-						break;
-				}
+		}
 }
