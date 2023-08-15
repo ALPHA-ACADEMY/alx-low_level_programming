6ea664aa882176f84cb11848a9035b9e2e5fa59a
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-15 03:54:27 -0700

    print the alphabel function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - function that prints the alphabet,
@@ -7,7 +7,7 @@
  * Return: Always 0.
 */
 
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
