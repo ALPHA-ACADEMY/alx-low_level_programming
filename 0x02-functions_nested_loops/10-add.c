02f397506c7a442d2a0fdb9c9bf165fc5a253254
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-15 11:34:31 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - finction that adds two integers and returns the result.
+ * @i: numbers that we are gonna add together.
+ * @j: same as i.
+ *
+ * Return: the sum of two integers.
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
