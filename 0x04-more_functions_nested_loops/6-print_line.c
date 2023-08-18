9e9b2b02b039fff62fc5a88069c07fc2fd4d8c13
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-18 04:52:02 -0700

    draws a straight in the terminal depending how number on n

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,14 +4,15 @@
  * print_line - function that draws a straight line in the terminal.
  * @n: number of line.
  *
- * Return: nothing because it is void.*/
+ * Return: nothing because it is void.
+*/
 
 void print_line(int n)
 {
 	int i;
 
 	if (n <= 0)
-	       _putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
