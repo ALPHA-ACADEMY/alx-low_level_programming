45e3324b6888b15bc0b2e837c435340ad90af0f3
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-21 10:44:48 -0700

    print reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,10 @@ void print_rev(char *s)
 	int len = 0;
 	int i;
 
-	while (s[len++]);
+	while (s[len])
+		len++;
 
-	for (i = len - 2; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
-	_putchar('\n');		
+	_putchar('\n');
 }
