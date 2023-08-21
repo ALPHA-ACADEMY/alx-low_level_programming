109a28fdb102923f8272e55ca7b2dbe8381d7d0e
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-21 11:56:28 -0700

    take one and avoid second , keep going

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - function that print the first character,
+* avoid the second and so so.
+* @str: string that we cut in half
+* Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i=-1;
+
+	while (str[++i])
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+}
