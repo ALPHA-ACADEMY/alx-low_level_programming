68c41f8c14b15d7661918dee6b64f6c4a41bd02a
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-23 09:34:31 -0700

    function that concatenates two strings and using n parameters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,8 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i++])
 		dest_len++;
 
-	for (i = 0; i < n; i++)
-		dest[dest_len] = src[i];
+	for (i = 0; i < n && src[i]; i++)
+		dest[dest_len++] = src[i];
 
 	return (dest);
 }
