54ded596c751d2ae4c1793aaa077314711db6f12
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-23 10:17:51 -0700

    function that copies a string with n parameter

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strncpy - function that copies a string.
+* @des: the string
+* @src: the string
+* @n: the number of bytes from src to be appended to des.
+*
+* Return: A pinter to the resulting string dest.
+**/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int src_len = 0;
+
+	while (src[i++])
+		src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = src_len; i < n; i++)
+		dest[index] = '\0';
+
+	return (dest);
+}
