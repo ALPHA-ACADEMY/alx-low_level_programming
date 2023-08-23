ce7b662be717e9d234be840e3bd95cd6c50280c5
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-23 11:06:07 -0700

    restart

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,4 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int dest_len = 0;
-
-	while (dest[i++])
-		dest_len++;
-
-	for (i = 0; i < n && src[i]; i++)
-		dest[dest_len++] = src[i];
-
-	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strcmp - function that compares two strings.
+* @s1 : first string.
+* @s2 : second string.
+*
+* Return: integer;
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+}
