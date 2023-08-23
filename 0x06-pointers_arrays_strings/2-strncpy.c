d2a50c02828ccf86c93d6636355aa92a5748e7bf
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-23 10:41:22 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 	int src_len = 0;
 
 	while (src[i++])
@@ -21,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 
 	for (i = src_len; i < n; i++)
-		dest[index] = '\0';
+		dest[i] = '\0';
 
 	return (dest);
 }
