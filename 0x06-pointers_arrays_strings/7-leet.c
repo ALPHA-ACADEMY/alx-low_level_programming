4d6d3f0278e14eda8af89b1480e8dc575c2dfdbb
ABDERRAHIM <benmhandcreative@gmail.com>
2023-08-24 10:54:28 -0700

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,16 @@
 
 char *leet(char *str)
 {
-	int i = 0; j;
+	int i = 0, j;
+
 	char leet[8] = {'0', 'L', '°', 'E', 'A', '°', '°', 'T'};
 
 	while (str[i])
 	{
 		for (j = 0; j <= 7; j++)
 		{
-			if(str[i] == leet[j] || str[i] - 32 == leet[j])
+			if (str[i] == leet[j] ||
+			   str[i] - 32 == leet[j])
 				str[i] = j + '0';
 		}
 		i++;
