250991ab78ac6cd2ffec6d78eee3d4e9bd04128c
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-08-29 03:35:52 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-/***/
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @s: string to be scanned
+ * @accept: string containing characters to matches.
+ * Return: pointer to the bytes in s that match one bytes in accept.
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
