eaede51e737fed553c18701516d52039a950b2a7
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-02 02:57:06 -0700

    push it like always

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
- * */
+ * main - program that prints the number of arguments passed into it.
+ * @argc: number of argument or count.
+ * @argv: array of argument.
+ * Return: 0 on Success.
+*/
 
 int main(int argc, char **argv)
 {
