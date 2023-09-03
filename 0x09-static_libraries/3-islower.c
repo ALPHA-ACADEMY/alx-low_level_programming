1d22eab20bbfd0c7cb5822a46818f64efc1c882d
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-03 08:53:42 -0700

    my static library task 0

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - a function that checks for lowercase character.
- * @c : The caracter to be checked
- *
- * Return: 1 if c is lowercase, 0 otherwise.
-*/
-
-int _islower(char c)
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,16 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - function that checks for alphabetic character.
- * @c: the character to be checked.
- *
- * Return: 1 if c is a letter LOWER or UPPER, 0 otherwise
-*/
-
-int _isalpha(char c)
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') ||  (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
