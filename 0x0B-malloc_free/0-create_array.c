7a717f6a91a39b38bbb95f9cb62c5c408b188f69
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-04 11:57:33 -0700

    Task 0 create a dynamique array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,14 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i = 0;
 
-	array = (char *) malloc(size * sizeof(char));
-
-	if (size == 0 || array == NULL)
+	if (size == 0)
 		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
