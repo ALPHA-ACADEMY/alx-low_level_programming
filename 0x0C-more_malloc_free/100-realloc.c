e92f04ecbe3d9e60fca51cfc01fc71c24228e1f0
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-07 08:45:35 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,8 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *tmp, *fill;
 
-	if (new_size > old_size)
-		return (NULL);
+	if (new_size == old_size)
+		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
