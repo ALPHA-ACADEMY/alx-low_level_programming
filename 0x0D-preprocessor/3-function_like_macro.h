4f069dcd33e0d37f55beca7da7c8b904ebe74758
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-11 10:09:17 -0700

    using functions as macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-/**
- * File: 3-function_like_macro.h
- * Auth: Abderrahim BENMHAND
- * Desc: Header file that defines a function-like macro ABS(x)
- *	that computes the absolute value of a number x.
-*/
-
-#define ABS(x) ((x) < (0) ? (-x) : (x));
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
