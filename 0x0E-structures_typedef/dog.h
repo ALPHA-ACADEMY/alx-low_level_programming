06ffe49aec882154638fc30e7a6fecc654dd2b54
ALPHA-ACADEMY <benmhandcreative@gmail.com>
2023-09-11 11:28:09 -0700

    new type Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - A new type named with a dog.
+ * struct dog - A new type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
