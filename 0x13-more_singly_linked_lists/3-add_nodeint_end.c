a427fa42a99c62b784570461cd83ffda00be59b6
Abderrahim with Loubna <benmhandcreative@gmail.com>
2023-09-26 07:05:38 -0700

    Hot Fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		lastNode = *head;
-		while(lastNode->next != NULL)
+		while (lastNode->next != NULL)
 			lastNode = lastNode->next;
 		lastNode->next = newNode;
 	}
