c6a53c74999807af2183a486baa36494c0f591c9
Abderrahim with Loubna <benmhandcreative@gmail.com>
2023-09-26 08:01:36 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(copy);
 		return (1);
 	}
-	for (i=0; i < (i - 1); i++)
+	for (i=0; i < (index - 1); i++)
 	{
 		if (copy->next == NULL)
 			return (-1);
