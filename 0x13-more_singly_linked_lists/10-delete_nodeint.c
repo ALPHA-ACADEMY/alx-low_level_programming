0a025fecf5d6c0c8affb3fee9a4a0df407971328
Abderrahim with Loubna <benmhandcreative@gmail.com>
2023-09-26 08:04:05 -0700

    Hot Fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,13 +15,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (copy == NULL)
 		return (-1);
 
-	if (i == 0)
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(copy);
 		return (1);
 	}
-	for (i=0; i < (index - 1); i++)
+	for (i = 0; i < (index - 1); i++)
 	{
 		if (copy->next == NULL)
 			return (-1);
